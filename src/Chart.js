fadb57e7e1702e98226ff2a1a9fdebb61b9b76bd
serenity <registredmail@mail.ru>
2018-04-09 01:45:46 +0300

    added functionality to change the d property

--- src/Chart.js
+++ src/Chart.js
@@ -3,8 +3,14 @@ import Axes from './Axes.js';
 
 export default class Chart {
     constructor(id, data = []) {
+        this.startDate = 1490178420000;
         this.data = data;
-        this.margin = { top: 20, right: 20, bottom: 20, left: 20 };
+        this.margin = {
+            top: 20,
+            right: 20,
+            bottom: 20,
+            left: 20,
+        };
         this.width = Math.min(window.outerWidth, 1000);
         this.height = 420;
 
@@ -49,5 +55,52 @@ export default class Chart {
             .attr('transform', `translate(0, ${this.margin.top})`)
             .attr('fill', '#02a6f2')
             .attr('fill-opacity', 0.1);
+
+        const rangeValue = [0, this.width - this.margin.right];
+        setInterval(() => {
+            this.line.attr('d', this.getLineData.bind(this));
+            this.area.attr('d', this.getAreaData.bind(this));
+            this.axes.x = d3.scaleTime()
+                .range(rangeValue)
+                .domain(d3.extent(this.data, (d) => d.date));
+        }, 1000);
+    }
+
+    /**
+     * return data string
+     * @returns {string}
+     */
+    getLineData() {
+        return this.lineFunc(this.generateNewData());
+    }
+
+    /**
+     * return data string
+     * @returns {string}
+     */
+    getAreaData() {
+        return this.areaFunc(this.generateNewData(true));
+    }
+
+    /**
+     * get json with new element
+     * @param isSkipDateIncrease
+     * @returns {Array}
+     */
+    generateNewData(isSkipDateIncrease = false) {
+        const getNumb = () => parseFloat((Math.random() * (57.9 - 58) + 58).toFixed(3));
+        const increaseValue = 2 * 60000;
+        const updData = () => {
+            this.data.push({
+                date: (isSkipDateIncrease ? this.startDate : this.startDate += increaseValue),
+                open: getNumb(),
+                max: getNumb(),
+                min: getNumb(),
+                close: getNumb(),
+            });
+            this.data.shift();
+            return this.data;
+        };
+        return updData();
     }
 }
